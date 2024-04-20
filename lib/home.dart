abb32ec1dbe916f577d6a05bd78f0e91b717c5a2
sergioIbgz <sergioibagnez@gmail.com>
2024-04-20 16:14:05 -0500

    error

--- lib/home.dart
+++ lib/home.dart
@@ -1,64 +1,70 @@
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Home extends StatelessWidget {
+import 'gyroscope_provider.dart';
+
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final gyroscope$ = ref.watch(gyroscopeProvider);
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          width: 320,
-          height: 250,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Stack(
-              clipBehavior: Clip.hardEdge,
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  width: 320,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    color: Colors.amber,
-                    image: const DecorationImage(
-                      image: AssetImage('assets/osaka-sky.png'),
-                      fit: BoxFit.none,
+        child: gyroscope$.when(
+          data: (data) => SizedBox(
+            width: 320,
+            height: 250,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Stack(
+                clipBehavior: Clip.hardEdge,
+                alignment: Alignment.center,
+                children: [
+                  AnimatedContainer(
+                    width: 320,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      image: DecorationImage(
+                        image: const AssetImage('assets/osaka-sky.png'),
+                        fit: BoxFit.none,
+                        alignment: Alignment(data.x * 0.1, data.y * 0.1),
+                      ),
+                    ),
+                    duration: const Duration(milliseconds: 1000),
+                  ),
+                  AnimatedPositioned(
+                    top: 20 + (21 * data.y),
+                    duration: const Duration(milliseconds: 500),
+                    child: Text(
+                      'OSAKA',
+                      style: TextStyle(
+                        fontSize: 90,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.4),
+                            blurRadius: 10,
+                            offset: const Offset(5, 5),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  top: 20,
-                  child: Text(
-                    'OSAKA',
-                    style: TextStyle(
-                      fontSize: 90,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black.withOpacity(0.4),
-                          blurRadius: 10,
-                          offset: const Offset(5, 5),
-                        ),
-                      ],
+                  AnimatedPositioned(
+                    bottom: -130 + data.x * 20,
+                    right: -10 + data.x * 20,
+                    duration: const Duration(milliseconds: 500),
+                    child: Image.asset(
+                      'assets/osaka-tower.webp',
+                      height: 500,
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: -130,
-                  right: -10,
-                  child: Image.asset(
-                    'assets/osaka-tower.webp',
-                    height: 500,
-                  ),
-                ),
-                Positioned(
+                  Positioned(
                     bottom: 0,
                     child: Container(
                       width: 320,
@@ -99,10 +105,21 @@ class Home extends StatelessWidget {
                           ),
                         ),
                       ),
-                    ))
-              ],
+                    ),
+                  ),
+                  Text(
+                    '$data',
+                    style: const TextStyle(
+                      color: Colors.amber,
+                      fontSize: 30,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
+          error: (error, stackTrace) => Text('$error'),
+          loading: () => const CircularProgressIndicator(),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:osaka/home.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const MyApp());
+import 'home.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
+  return runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
